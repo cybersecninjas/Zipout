5d55da0184db00ba1366e6fbe504895586b9aa1b
John Ombagi <jayombagi@gmail.com>
2019-04-05 22:28:38 +0300

    Add exception handlers, threading, comments and refactor code

--- zipCracker.py
+++ zipCracker.py
@@ -1,24 +1,60 @@
-##Import zipfile
+import os
+import sys
+import zipfile
+import threading
 
-import  zipfile
+def helper():
+	'''Print Help Message to user.'''
+	print '[!] This is help message. Provide the application with the following parameters:'
+	print '[+] Please follow the order provided (zip file then wordlist)'
+	print '\t[-] %s file.zip wordlist.txt' % sys.argv[0]
 
-##initialize the protected zipfile
+def cracker(password,file_to_open):
+	'''Test if a password can crack a zip file.'''
+	try:
+		file_to_open.extractall(pwd=password)
+		victory_message = '\n[!] Password found: %s \n' % password
+		print victory_message
+		os._exit(0)
+	except Exception as err:
+		pass
 
-file_Name = "help.zip"
+# Check if parameters are enough before starting
+if len(sys.argv) < 3 :
+	helper()
+	sys.exit()
+else:
+	file_to_crack = sys.argv[1]
+	dictionary = sys.argv[2]
 
-##initialize your payloads
 
-dictionary = "rockyou.txt"
+# Check if Zip file can be accessed
+try:
+	file_to_open = zipfile.ZipFile(file_to_crack)
+except IOError:
+	print '[!] An Error Ocured While reading the provided Zip File: %s' % file_to_crack
+	sys.exit()
+except zipfile.BadZipfile:
+	print '[!] You provided a bad Zip file: %s' % file_to_crack
+	sys.exit()
 
-password = ' '
 
-file_to_open = zipfile.ZipFile(help.zip)
-with open(dictionary, 'r') as f:
-	for line in f.readlines():
-		try:
-			file_to_open.extractall(pwd=password)
-			password = 'password found: %s' % password
-			print password
-		except:
-			pass
+# Alert User Cracking started
+print '[+] Zip Cracking Process started:'
+print '\t[-] Cracking File : %s' % file_to_crack
+print '\t[-] Using Wordlist : %s' % dictionary
 
+
+# Test each word in the passwordlist
+try:
+	with open(dictionary, 'r') as f:
+		for line in f.readlines():
+			try_cracking = threading.Thread(target=cracker, args=(line.strip(),file_to_open))
+			try_cracking.start()
+			try_cracking.join()
+except IOError as err:
+	print '[!] An Error Ocured While reading the provided dictionary file: %s' % dictionary
+	sys.exit()
+else:
+	print '[!] Something went wrong while starting the bruteforce: %s' % dictionary
+	sys.exit()
